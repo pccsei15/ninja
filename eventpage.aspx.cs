5089ba47bb7350a1d1a39151f62bfa1f12215e80
joshbnntt <jbenne2543@students.pcci.edu>
2014-09-19 16:02:40 -0500

    gridview

--- eventpage.aspx.cs
+++ eventpage.aspx.cs
@@ -4,16 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-// My random comment
+using System.Data.SqlClient;
 
 namespace ProjectNinja
 {
-   public partial class eventpage : System.Web.UI.Page
-   {
-      protected void Page_Load(object sender, EventArgs e)
-      {
+    public partial class eventpage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //using (SqlConnection thisConnection = new SqlConnection("Server=172.16.212.212;Database=SEI_Ninja;User ID=999999;Password=99999999;Trusted_Connection=True;"))
+            //{
+            //    thisConnection.Open();
 
-      }
-   }
+            //    using (SqlCommand thisCommand = thisConnection.CreateCommand())
+            //    {
+            //        thisCommand.CommandText = "SELECT * FROM [SEI_Ninja].[dbo].[SCHEDULED_USERS] NschedUsers JOIN [SEI_TimeMachine2].[dbo].[USER] TMusers ON TMusers.user_id = NschedUsers.userID JOIN [SEI_Ninja].[dbo].[EVENT_TIMES] Ntime ON NschedUsers.eventTimeID = Ntime.eventTimeID JOIN [SEI_Ninja].[dbo].[EVENT] Nevent ON Nevent.eventID = Ntime.eventID WHERE NschedUsers.userID = 113920;";
+
+            //        using (SqlDataReader thisReader = thisCommand.ExecuteReader())
+            //        {
+            //            while (thisReader.Read())
+            //            {
+            //                //MessageBox.Show(thisReader["user_first_name"].ToString());
+            //                string display = "Pop-up!";
+            //                ClientScript.RegisterStartupScript(this.GetType(), "yourMessage", "alert('" + display + "');", true);
+            //            }
+            //        }
+            //    }
+            //}
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            //if (_text == null)
+            //   _text = "Here is some default text.";
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //needs code here
+        }
+
+
+
+
+    }
 }
+
