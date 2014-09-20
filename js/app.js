d89e97346e31ba7a012eeb7c53c3067f83adbe0d
leahgrace <lj3692@yahoo.com>
2014-09-20 15:47:50 -0500

    Merging repo with both ProjectNinja folders on csmain

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// DataTables Functions
+
+$(document).ready(function () {
+    $('#events-table').dataTable({
+      "paging": false,
+      "columns": [
+          { "orderable": false },
+          null,
+          null,
+          null,
+          null,
+          { "orderable": false }
+        ],
+      "order": [2, 'asc'],
+      "info": false
+    });
+
+} );
+
+// DatePicker Functions
+
+$(function() {
+  $('#datepicker').datepicker();
+});
+
+// Start the time and date picker
+$(function () {
+    $('#eventDate').datepicker({
+        todayHighlight: true,
+        todayBtn: "linked"
+    });
+});
