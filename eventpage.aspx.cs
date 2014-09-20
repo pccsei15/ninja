e224b6b0b1c85c3a7b9da8fae761464289d9a5d3
leahgrace <lj3692@yahoo.com>
2014-09-20 15:47:50 -0500

    Merging repo with both ProjectNinja folders on csmain

---
+++ eventpage.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ProjectNinja
+{
+   public partial class eventpage : System.Web.UI.Page
+   {
+      protected void Page_Load(object sender, EventArgs e)
+      {
+          using (SqlConnection thisConnection = new SqlConnection("Server=172.16.212.212;Database=SEI_Ninja;User ID=999999;Password=99999999;Trusted_Connection=True;"))
+          {
+              thisConnection.Open();
+
+              using (SqlCommand thisCommand = thisConnection.CreateCommand())
+              {
+                  thisCommand.CommandText = "SELECT * FROM [SEI_Ninja].[dbo].[SCHEDULED_USERS] NschedUsers JOIN [SEI_TimeMachine2].[dbo].[USER] TMusers ON TMusers.user_id = NschedUsers.userID JOIN [SEI_Ninja].[dbo].[EVENT_TIMES] Ntime ON NschedUsers.eventTimeID = Ntime.eventTimeID JOIN [SEI_Ninja].[dbo].[EVENT] Nevent ON Nevent.eventID = Ntime.eventID WHERE NschedUsers.userID = 113920;";
+
+                  using (SqlDataReader thisReader = thisCommand.ExecuteReader())
+                  {
+                      while (thisReader.Read())
+                      {
+                         // MessageBox.Show(thisReader["user_first_name"].ToString());
+                      }
+                  }
+              }
+          }
+      }
+   }
+}
