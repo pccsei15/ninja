2d5be3826b488b03ef154eb574914e3af2278cf5
Skywings21 <doryneal@geusnet.com>
2014-09-25 16:34:03 -0500

    Add back GridViewFix that is needed
    This file was deleted and should not have been, in trying to find out
    where the search is broken added back a jquery

---
+++ js/GridviewFix.js
@@ -0,0 +1,11 @@
+﻿//GridviewFix Plugin Code
+
+function fixGridView(tableEl) {
+    var jTbl = $(tableEl);
+
+    if (jTbl.find("tbody>tr>th").length > 0) {
+        jTbl.find("tbody").before("<thead><tr></tr></thead>");
+        jTbl.find("thead tr").append(jTbl.find("th"));
+        jTbl.find("tbody tr:first").remove();
+    }
+}
